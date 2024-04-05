ccf89e8917b71541488f43c9dc7863664234d664
Edgar Maldonado <ed.dev28@gmail.com>
2024-04-05 03:01:33 -0500

    Backup Add [Conex] and fix animations

--- js/script.js
+++ js/script.js
@@ -2,13 +2,19 @@ const nav = document.querySelector('#nav');
 const abrir = document.querySelector('#abrir');
 const cerrar = document.querySelector('#cerrar');
 
-window.onload = function() {
+/* ------ Register Module ------- */
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+const logo = document.getElementById('logo');
+
+window.onload = function () {
     window.scrollTo(0, 0);
 }
 
+
 function toggleNav() {
     nav.classList.add("nav-visible");
- 
     cerrar.classList.remove('reiniciar-spin');
     void cerrar.offsetWidth; // Trigger reflow
     cerrar.classList.add('reiniciar-spin');
@@ -30,12 +36,20 @@ cerrar.addEventListener('click', () => {
     document.body.style.overflow = 'scroll';
     void cerrar.offsetWidth; // Trigger reflow
     cerrar.classList.add('reiniciar-spin');
-  
+
 })
 
 
 
-function test() {
-    nav.classList.remove("nav-visible");
-    document.body.style.overflow = 'scroll';
-}
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+
+logo.addEventListener("click", function() {
+    // Redirigir a una URL cuando se hace clic en el título h4
+    window.location.href = "index.html";
+});
